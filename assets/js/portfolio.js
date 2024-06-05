eaf0d5066fd21b1c3f0288ef9fd2bbc8bff6f59d
moad hrm <moadehrimech22@gmail.com>
2024-06-05 18:26:14 +0100

    start changes

---
+++ assets/js/portfolio.js
@@ -0,0 +1,307 @@
+var portfolioData = [
+  {
+    key: "0",
+    category: "LARAVEL",
+    title: "DarirGigs",
+    type: "Personal Project",
+    imageUrl: "assets/img/portfolio/laravel/darir-gigs/main.png",
+    images: [
+      "assets/img/portfolio/laravel/darir-gigs/hero.png",
+      "assets/img/portfolio/laravel/darir-gigs/register.png",
+      "assets/img/portfolio/laravel/darir-gigs/single-gig.png",
+    ],
+    projectURL:
+      "https://github.com/DarirAbdelbassit/DarirGigs-Find-post-jobs-projects",
+    projectUrlTitle: "View on GitHub",
+    description:
+      "DarirGigs is a modern web application developed using Laravel and Tailwind CSS, serving as a centralized platform for job seekers and project enthusiasts. This user-friendly system allows visitors to explore, submit, and manage job or project listings seamlessly. Key features include the ability to browse through various opportunities, a powerful search functionality, and user authentication for registered individuals. Users can log in to submit new listings, edit existing posts, and manage their submissions effortlessly. With a focus on providing a streamlined experience, DarirGigs connects individuals with diverse opportunities in a convenient and efficient manner.",
+    technologies: ["HTML5", "Tailwind CSS", "JavaScript", "Laravel", "MySQL"],
+  },
+  {
+    key: "1",
+    category: "PHP",
+    title: "DarirSettaf Store",
+    type: "Final Project",
+    imageUrl: "assets/img/portfolio/php/e-commerce/main.jpg",
+    images: [
+      "assets/img/portfolio/php/e-commerce/hero.jpg",
+      "assets/img/portfolio/php/e-commerce/products.jpg",
+      "assets/img/portfolio/php/e-commerce/checkout.png",
+      "assets/img/portfolio/php/e-commerce/admin-add-product.png",
+      "assets/img/portfolio/php/e-commerce/UseCaseModel_Admin.jpg",
+    ],
+    projectURL: "https://github.com/DarirAbdelbassit/e-commerce",
+    projectUrlTitle: "View on GitHub",
+    description:
+      "DarirSettaf store is an E-commerce platform that enables visitors to explore a diverse range of electronic products. Visitors have the option to create accounts, granting them customer privileges such as contacting the administration and making purchases through the PayPal payment gateway. Ensuring a seamless experience, the platform features an admin dashboard for handling user inquiries, managing products, and overseeing coupon codes. This setup ensures efficient operations and customer satisfaction on the platform.",
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "Bootstrap",
+      "JavaScript",
+      "jQuery",
+      "PHP",
+      "MySQL",
+      "PayPal API",
+    ],
+  },
+  {
+    key: "2",
+    category: "LARAVEL",
+    title: "DarirMovies",
+    type: "Personal Project",
+    imageUrl: "assets/img/portfolio/laravel/darir-movies/main.png",
+    images: [
+      "assets/img/portfolio/laravel/darir-movies/movies-main.png",
+      "assets/img/portfolio/laravel/darir-movies/single-movie.png",
+      "assets/img/portfolio/laravel/darir-movies/tvshow-main.png",
+      "assets/img/portfolio/laravel/darir-movies/single-actor.png",
+    ],
+    projectURL: "https://github.com/DarirAbdelbassit/Movie-App-Laravel",
+    projectUrlTitle: "View on GitHub",
+    description:
+      "DarirMovies is a modern web application meticulously constructed using Laravel, Livewire, Alpine.js, and Tailwind CSS. Leveraging seamless integration with the TMDb API, the platform fetches and displays comprehensive movie and TV show data, providing users with an immersive experience. Enthusiasts can explore a diverse collection of content, from popular and now playing movies to top-rated TV shows. The application goes beyond by offering detailed profiles of popular actors, complete with social media links and an extensive list of their works and credits. DarirMovies stands as a feature-rich hub for cinephiles, seamlessly combining technology and entertainment to cater to the diverse tastes of movie and TV show enthusiasts.",
+    technologies: [
+      "HTML5",
+      "Tailwind CSS",
+      "Alpine.js",
+      "Laravel",
+      "Livewire 3",
+      "TMDb API",
+    ],
+  },
+  {
+    key: "3",
+    category: "PHP",
+    title: "ESEFA Attendance System",
+    type: "Internship Project",
+    imageUrl: "assets/img/portfolio/php/absence-manager/login.png",
+    images: [
+      "assets/img/portfolio/php/absence-manager/login.png",
+      "assets/img/portfolio/php/absence-manager/modules.png",
+      "assets/img/portfolio/php/absence-manager/students-list.png",
+    ],
+    projectURL: "https://github.com/DarirAbdelbassit/Gestion_Absence_ESEFA",
+    projectUrlTitle: "View on GitHub",
+    description:
+      "The ESEFA Absence Management System is an application designed for professors at The Higher School of Education and Training - Agadir. Professors can log in to their accounts to access a comprehensive overview of their modules and classes. Within this system, they can efficiently view the list of students in each class and manage attendance records for individual students. This application streamlines the process of tracking and  managing student absences.",
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "Bootstrap",
+      "JavaScript",
+      "jQuery",
+      "PHP",
+      "MySQL",
+    ],
+  },
+  {
+    key: "4",
+    category: "LARAVEL",
+    title: "Creer Mon Entreprise",
+    type: "Work Project",
+    imageUrl: "assets/img/portfolio/laravel/cme/main.png",
+    images: [
+      "assets/img/portfolio/laravel/cme/hero.png",
+      "assets/img/portfolio/laravel/cme/forms-list.png",
+    ],
+    projectURL: "https://creermonentreprise.net/",
+    projectUrlTitle: "Live demo",
+    description:
+      '"Cree Mon Entreprise" is a comprehensive platform designed to facilitate entrepreneurs in effortlessly creating businesses online in France. From assisting in determining the optimal type of enterprise for their specific case to generating all the necessary documents for establishing a business in France, the platform offers a seamless and user-friendly experience. The site is structured into four distinct areas, catering to different roles: customer, accountant, administrator, and super-administrator. This multifaceted platform aims to simplify the entrepreneurial journey, providing tailored support and resources for individuals navigating the process of establishing businesses in France.',
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "Bootstrap",
+      "Alpine.js",
+      "Laravel",
+      "Livewire 2",
+      "MySQL",
+    ],
+  },
+  {
+    key: "5",
+    category: "WordPress",
+    title: "Ecomayex",
+    type: "Freelance Project",
+    imageUrl: "assets/img/portfolio/wordpress/ecomayex/main.png",
+    images: [
+      "assets/img/portfolio/wordpress/ecomayex/home.png",
+      "assets/img/portfolio/wordpress/ecomayex/facebook-class.png",
+      "assets/img/portfolio/wordpress/ecomayex/master-class.png",
+    ],
+    projectURL: "https://ecomayex.com/",
+    projectUrlTitle: "Live demo",
+    description:
+      "Ecomayex is a specialized WordPress site offering two courses: Facebook and master classes, complemented by client reviews and results. The website details each course section and provides a straightforward application process, with the admin efficiently managing requests on a Google Sheet.",
+    technologies: [
+      "WordPress",
+      "Elementor",
+      "CSS3",
+      "JavaScript",
+      "WpForms",
+      "Google Sheets API",
+    ],
+  },
+  {
+    key: "6",
+    category: "LARAVEL",
+    title: "Votre Assistant Marketing Maroc",
+    type: "Work Project",
+    imageUrl: "assets/img/portfolio/laravel/vamm/hero.png",
+    images: [
+      "assets/img/portfolio/laravel/vamm/main.png",
+      "assets/img/portfolio/laravel/vamm/admin-dashboard.png",
+      "assets/img/portfolio/laravel/vamm/login.png",
+      "assets/img/portfolio/laravel/vamm/admin-statics.png",
+    ],
+    projectURL: "#",
+    projectUrlTitle: "Coming Soon",
+    description:
+      "'Votre Assistant Marketing Maroc' is a platform that allows you to create a dynamic electronic boutique for all products, with the ability to configure each attribute from the product to its specific attributes such as color, size, etc. through an interface for the admin to easily manage all aspects.",
+    technologies: [
+      "HTML5",
+      "CSS3",
+      "Bootstrap",
+      "Alpine.js",
+      "Laravel",
+      "Livewire 3",
+      "MySQL",
+    ],
+  },
+];
+var portfolioSwiper;
+document.addEventListener("DOMContentLoaded", function () {
+  function createPortfolioItem(item) {
+    return `
+        <div class="col-lg-4 col-md-6 portfolio-item filter-${item.category.toLowerCase()}" id="${item.key}">
+          <div class="portfolio-wrap">  
+            <img src="${item.imageUrl}" class="img-fluid" alt="" />
+            <div class="portfolio-info">
+              <h4>${item.title}</h4>
+              <h5>${item.type}</h5>
+              <div class="portfolio-links" onclick="showModal(${item.key})">
+                  <i class="bx bx-link" ></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+  }
+
+  function populatePortfolio() {
+    var portfolioContainer = document.getElementById("portfolio-container");
+
+    // Display all items in the shuffled array
+    portfolioData.forEach(function (item) {
+      var portfolioItem = createPortfolioItem(item);
+      portfolioContainer.innerHTML += portfolioItem;
+    });
+  }
+  populatePortfolio();
+});
+// the logic for the modal
+function showModal(id) {
+  var modal = document.querySelector(".portfolio-details");
+
+  // Add Animate.css class for fade-in aFnimation
+  modal.classList.add("animate__animated", "animate__fadeIn");
+
+  modal.style.display = "flex";
+
+  setTimeout(() => {
+    // Clear modal content
+    var modalTitle = document.querySelector(".portfolio-title");
+    modalTitle.innerHTML = "";
+
+    var projectInfo = document.querySelector(".portfolio-info ul");
+    projectInfo.innerHTML = "";
+
+    var projectDescription = document.querySelector(".description");
+    projectDescription.innerText = "";
+
+    var modalImageList = document.querySelector("#portfolio-images");
+    modalImageList.innerHTML = "";
+
+    var technologies = document.querySelector("#portfolio-used-technologies");
+    technologies.innerHTML = "";
+    // insert the data into the modal
+    var modalTitle = document.querySelector(".portfolio-title");
+    modalTitle.innerHTML = portfolioData[id].title;
+    var projectInfo = document.querySelector(".portfolio-info ul");
+    projectInfo.innerHTML = `
+    <li><strong>Category</strong>: ${portfolioData[id].category}</li>
+    <li><strong>Type</strong>: ${portfolioData[id].type}</li>
+    <li><strong>Project URL</strong>: <a target="_blank" href="${portfolioData[id].projectURL}" >${portfolioData[id].projectUrlTitle}</a></li>
+    `;
+    // Update project description
+    var projectDescription = document.querySelector(".description");
+    projectDescription.innerText = portfolioData[id].description;
+    // used technologies
+    var technologies = document.querySelector("#portfolio-used-technologies");
+    technologies.innerHTML = "";
+    portfolioData[id].technologies.forEach(function (item) {
+      var technology = document.createElement("span");
+      technology.innerText = item;
+      technologies.appendChild(technology);
+    });
+    // set the images to the src of the modal image list
+    var modalImageList = document.querySelector("#portfolio-images");
+
+    portfolioData[id].images.forEach(function (item) {
+      var swiperSlide = document.createElement("div");
+      swiperSlide.className = "swiper-slide";
+
+      var image = document.createElement("img");
+      image.src = item;
+
+      // Set maximum width and height
+      image.style.maxWidth = "100%";
+      image.style.maxHeight = "100%";
+
+      // Center the image within the container
+      image.style.display = "block";
+      image.style.margin = "auto";
+
+      swiperSlide.appendChild(image);
+      modalImageList.appendChild(swiperSlide);
+    });
+  }, 500);
+  /**
+   * Portfolio details slider
+   */
+  portfolioSwiper = new Swiper(".portfolio-details-slider", {
+    speed: 400,
+    loop: true,
+    rewind: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    deleteInstance: false,
+  });
+}
+
+function closeModal() {
+  var modal = document.getElementById("portfolio-details");
+
+  // Add Animate.css class for fade-out animation
+  modal.classList.add("animate__animated", "animate__fadeOut");
+
+  // Set a timeout to hide the modal after the animation completes
+  setTimeout(function () {
+    modal.style.display = "none";
+    // Remove both fade-in and fade-out classes to prepare for the next showing
+    modal.classList.remove("animate__fadeIn", "animate__fadeOut");
+  }, 500);
+  portfolioSwiper.destroy();
+}
